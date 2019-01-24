e3de63eeaaacd30534185741151cfea1fb09c481
unknown <motifridman5@gmail.com>
2019-01-24 16:04:29 +0200

    Add brodcast

--- public/js/index.js
+++ public/js/index.js
@@ -4,11 +4,6 @@ socket.on('connect', function () {
   console.log('Connected to server');
 });
 
-// socket.emit('createMessage', {
-//   from: 'Andrew',
-//   text: 'Hey. This is Andrew.'
-// });
-
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
